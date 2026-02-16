80eb16cebc5573b8e17de5a77eba030417937fc1
Arnav20007 <arnavsinghas1221@gmail.com>
2026-02-16 17:29:36 +0530

    Unignore and push dist folder for production serving

---
+++ dist/assets/formatters-CHLfW1ie.js
@@ -0,0 +1,1 @@
+function o(t){const r=Math.floor(t/12),n=t%12;return r===0?`${n} month${n!==1?"s":""}`:n===0?`${r} year${r!==1?"s":""}`:`${r} year${r!==1?"s":""} ${n} month${n!==1?"s":""}`}export{o as f};
